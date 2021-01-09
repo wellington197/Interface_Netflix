8d1c11c36290ddc064c7339327e5341636b4b69d
wellington197 <fcowellcosta@gmail.com>
2021-01-08 23:23:37 -0300

    Criação dos testes unitários

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Teste da aplicação do projeto NETFLIX")
+        self.assertRegex(result.data.decode(), "Interface Netflix")
 
 
 if __name__ == "__main__":

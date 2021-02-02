ad04e553f9ac987a32373c7af50d1b6d29bf2dc1
Wellington Costa <fcowellcosta@gmail.com>
2021-02-01 22:00:18 -0300

    Inserção do diretório para subir o index.html

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('/templates/index.html')
+    return render_template('./templates/index.html')
 
 
 if __name__ == '__main__':

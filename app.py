5df18ee1e96ee799356c97ca4c7c688d2ae232f4
Wellington Costa <fcowellcosta@gmail.com>
2021-04-20 11:55:49 -0300

    Alter folder static for assets

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-    return render_template('login.html')
-    return render_template('perfil_usuario.html')
-    return render_template('tela_inicial.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)

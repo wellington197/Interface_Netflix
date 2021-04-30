8a79617f904ea72e6b97a9df1a36c31ea1e72533
Wellington Costa <fcowellcosta@gmail.com>
2021-04-29 21:49:56 -0300

    Inserting apps to internal pages (login,tela_inicial,perfil_usuario)

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,request,redirect,session,flash
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/")
+def login():
+    return render_template('login.html')
+
+@app.route("/")
+def tela_inicial():
+    return render_template('tela_inicial.html')
+
+@app.route("/")
+def tela_inicial():
+    return render_template('perfil_usuario.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

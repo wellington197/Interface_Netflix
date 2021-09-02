3eef81e0c2bce9f8bf1777a4a4d2da5b83a8a6e5
wellington197 <fcowellcosta@gmail.com>
2021-09-02 15:00:15 -0300

    update app.pyl

--- app.py
+++ app.py
@@ -6,17 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
-
-@app.route("/tela_inicial")
-def tela_inicial():
-    return render_template('tela_inicial.html')
-
-@app.route("/perfil_usuario")
-def perfil_usuario():
-    return render_template('perfil_usuario.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
